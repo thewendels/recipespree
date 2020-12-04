820620263835fe4165e0b227944f098aacb8d2b6
Wendy <69648973+thewendels@users.noreply.github.com>
2020-12-04 14:21:31 -0600

    adds recipes create route and controller

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -5,6 +5,26 @@ class Api::RecipesController < ApplicationController
     render 'index.json.jb'
   end
 
+  def create
+    @recipe = Recipe.new({
+      name: params[:name],
+      source: params[:source],
+      recipe_url: params[:recipe_url],
+      servings: params[:servings],
+      total_prep_time: params[:total_prep_time],
+      intro: params[:intro],
+      ingredients: params[:ingredients],
+      instructions: params[:instructions],
+      notes: params[:notes],
+      image_url: params[:image_url]
+    })
+    if @recipe.save
+      render 'show.json.jb'
+    else
+      render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def show
     @recipe = Recipe.find_by(id: params[:id])
     render 'show.json.jb'
