1f290078b63b7db4626d8418652520d428c444b2
Wendy <69648973+thewendels@users.noreply.github.com>
2020-12-04 15:02:27 -0600

    adds users create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :recipe_tags
   has_many :recipes, through: :recipe_tags
   has_many :tags, through: :recipe_tags
@@ -6,5 +8,4 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: {in: 3..20}
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {in: 8..20}, confirmation: true
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+    
     get "/recipes" => "recipes#index"
     get "/recipes/:id" => "recipes#show"
     post "/recipes" => "recipes#create"
