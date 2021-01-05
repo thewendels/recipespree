c7c4ea920ad7fc0ac6c2b7927717e2feb6f85bc3
Wendy <69648973+thewendels@users.noreply.github.com>
2021-01-04 20:31:36 -0600

    adds total prep time numericality validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,7 @@ class Recipe < ApplicationRecord
 
   validates :user_id, presence: true
   validates :name, presence: true
+  validates :total_prep_time, numericality: { only_integer: true, greater_than: 0}
   validates :ingredients, presence: true
   validates :instructions, presence: true
 
